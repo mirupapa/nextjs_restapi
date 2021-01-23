f5a6ce81cf417b3d1111089ed8f4e56212b74944
mirupapa <mirutiropapa@icloud.com>
2021-01-23 22:05:46 +0900

    added host url

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mirupapa-nextjs-api.herokuapp.com']
 
 
 # Application definition
